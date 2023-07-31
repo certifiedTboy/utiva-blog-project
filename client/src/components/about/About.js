cb56f271a0b97a884b746e9d1333060392432bf8
certifiedTboy <79862233+certifiedTboy@users.noreply.github.com>
2023-07-31 23:01:34 +0100

    update css

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -77,48 +77,6 @@ const About = () => {
                     </div>
                   </div>
                 </div>
-                {/* <div className="col-lg-6 col-md-6 mb-5 mb-md-5">
-                  <div className="list-box d-flex">
-                    <div className="list-icon">
-                      <i className="fas fa-money-bill-alt"></i>
-                    </div>
-                    <div className="content">
-                      <h3>Best Price</h3>
-                      <p>
-                        sit voluptatem accusantium dolore mque laudantium, totam
-                        rem aperiam, eaque ipsa quae ab illo.
-                      </p>
-                    </div>
-                  </div>
-                </div> */}
-                {/* <div className="col-lg-6 col-md-6 mb-5 mb-md-5">
-                  <div className="list-box d-flex">
-                    <div className="list-icon">
-                      <i className="fas fa-briefcase"></i>
-                    </div>
-                    <div className="content">
-                      <h3>Custom Box</h3>
-                      <p>
-                        sit voluptatem accusantium dolore mque laudantium, totam
-                        rem aperiam, eaque ipsa quae ab illo.
-                      </p>
-                    </div>
-                  </div>
-                </div> */}
-                {/* <div className="col-lg-6 col-md-6">
-                  <div className="list-box d-flex">
-                    <div className="list-icon">
-                      <i className="fas fa-sync-alt"></i>
-                    </div>
-                    <div className="content">
-                      <h3>Quick Refund</h3>
-                      <p>
-                        sit voluptatem accusantium dolore mque laudantium, totam
-                        rem aperiam, eaque ipsa quae ab illo.
-                      </p>
-                    </div>
-                  </div>
-                </div> */}
               </div>
             </div>
           </div>
