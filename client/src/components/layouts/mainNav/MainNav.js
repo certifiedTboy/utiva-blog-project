862c6b0e231bf2021d41f38015e49e18180b784c
certifiedTboy <79862233+certifiedTboy@users.noreply.github.com>
2023-08-01 08:34:25 +0100

    update nav

--- client/src/components/layouts/mainNav/MainNav.js
+++ client/src/components/layouts/mainNav/MainNav.js
@@ -88,20 +88,7 @@ const MainNav = ({ scrollTop }) => {
                 Blogs
               </NavLink>
 
-              <Link
-                activeClass="active"
-                className={`nav-item nav-link ${classes.mousePoint} d-none d-sm-none d-md-block`}
-                to="contact"
-                spy={true}
-                smooth={true}
-                hashSpy={true}
-                duration={500}
-                delay={500}
-                isDynamic={true}
-                ignoreCancelEvents={false}
-                spyThrottle={500}>
-                Contact
-              </Link>
+        
             </div>
             {!user && (
               <li className={`nav-link nav-item d-none d-sm-none d-md-block`}>
@@ -114,14 +101,14 @@ const MainNav = ({ scrollTop }) => {
                 </NavLink>
               </li>
             )}
-            {/* <form className="d-flex d-none d-sm-none d-md-block" role="search">
+            <form className="d-flex d-none d-sm-none d-md-block" role="search">
               <input
                 class="form-control me-2 d-none d-sm-none d-md-block"
                 type="search"
                 placeholder="Search"
                 aria-label="Search"
               />
-            </form> */}
+            </form>
 
             {user && (
               <li
@@ -216,33 +203,7 @@ const MainNav = ({ scrollTop }) => {
             </li>
           )}
 
-          {!show && (
-            <li className="nav-item">
-              <Link
-                activeClass="active"
-                className={`nav-item nav-link ${classes.mousePoint} ${classes.nav_link}`}
-                to="contact"
-                spy={true}
-                smooth={true}
-                hashSpy={true}
-                duration={500}
-                delay={500}
-                isDynamic={true}
-                ignoreCancelEvents={false}
-                spyThrottle={500}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="26"
-                  height="26"
-                  fill="#F28123"
-                  class="bi bi-person-lines-fill"
-                  viewBox="0 0 16 16">
-                  <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z" />
-                </svg>
-              </Link>
-            </li>
-          )}
-
+          
           {!show && (
             <li className="nav-item dropdown">
               <Nav.Link
