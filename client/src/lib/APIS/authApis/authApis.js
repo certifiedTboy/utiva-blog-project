c71a5a4479252982fc5f82947d2f9e7e74a04e17
certifiedTboy <79862233+certifiedTboy@users.noreply.github.com>
2023-08-03 14:11:52 +0100

    infinite scroll

--- client/src/lib/APIS/authApis/authApis.js
+++ client/src/lib/APIS/authApis/authApis.js
@@ -1,11 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { userApi } from "../userApi/userApi";
 import { logout } from "../userApi/redux/UserSlice";
-
+// https://utivablog-project-server.onrender.com/api/v1/
 export const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://utivablog-project-server.onrender.com/api/v1/",
+    baseUrl: "http://localhost:8000/api/v1/",
   }),
   endpoints: (builder) => ({
     loginUser: builder.mutation({
