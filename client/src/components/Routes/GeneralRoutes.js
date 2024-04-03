fb86903a3c65b57f5effb48497c92b09fddbe38f
certifiedTboy <etosin70@gmail.com>
2024-04-03 16:14:46 +0100

    update router

--- client/src/components/Routes/GeneralRoutes.js
+++ client/src/components/Routes/GeneralRoutes.js
@@ -1,31 +1,53 @@
-import React from "react";
+import { lazy, Suspense } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { ProtectedRoutes, AdminProtectedRoutes } from "./ProtectedRoutes";
-import HomePage from "../../pages/HomePage";
-import AboutPage from "../../pages/AboutPage";
-import ErrorPage from "../../pages/ErrorPage";
-import AllBlogPage from "../../pages/AllBlogPage";
-import AuthPage from "../../pages/AuthPage";
-import ProfilePage from "../../pages/ProfilePage";
-import SingleBlogPage from "../../pages/SingleBlogPage";
 import Login from "../auth/Login";
 import Register from "../auth/Register";
 import SetPassword from "../auth/SetPassword";
 import RequestResetPassword from "../auth/RequestResetPassword";
 import SetNewRequestPassword from "../auth/SetNewRequestPassword";
-import PostBuilderPage from "../../pages/PostBuilderPage";
-import AdminPage from "../../pages/AdminPage";
+
+const HomePage = lazy(() => import("../../pages/HomePage"));
+const AboutPage = lazy(() => import("../../pages/AboutPage"));
+const ErrorPage = lazy(() => import("../../pages/ErrorPage"));
+const AllBlogPage = lazy(() => import("../../pages/AllBlogPage"));
+const AuthPage = lazy(() => import("../../pages/AuthPage"));
+const ProfilePage = lazy(() => import("../../pages/ProfilePage"));
+const SingleBlogPage = lazy(() => import("../../pages/SingleBlogPage"));
+const PostBuilderPage = lazy(() => import("../../pages/PostBuilderPage"));
+const AdminPage = lazy(() => import("../../pages/AdminPage"));
 
 const GeneralRoutes = () => {
   const { user } = useSelector((state) => state.userState);
 
   return (
     <Routes>
-      <Route path="*" element={<ErrorPage />} />
+      <Route
+        path="*"
+        element={
+          <Suspense fallback={<></>}>
+            <ErrorPage />
+          </Suspense>
+        }
+      />
       <Route path="/" element={<Navigate to="/home" replace={true} />} exact />
-      <Route path="/home" element={<HomePage />} />
-      <Route path="/get-started" element={<AuthPage />}>
+      <Route
+        path="/home"
+        element={
+          <Suspense fallback={<></>}>
+            <HomePage />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/get-started"
+        element={
+          <Suspense fallback={<></>}>
+            <AuthPage />
+          </Suspense>
+        }
+      >
         <Route path="sign-up" element={<Register />} />
         <Route path="sign-in" element={<Login />} />
         <Route path="reset-password" element={<RequestResetPassword />} />
@@ -38,32 +60,68 @@ const GeneralRoutes = () => {
           element={<SetNewRequestPassword />}
         />
       </Route>
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/blogs" element={<AllBlogPage />} />
-      <Route path="/w-d/:username" element={<ProfilePage />} />
-      <Route path="/blogs/:blogTitle" element={<SingleBlogPage />} />
+      <Route
+        path="/about"
+        element={
+          <Suspense fallback={<></>}>
+            <AboutPage />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/blogs"
+        element={
+          <Suspense fallback={<></>}>
+            <AllBlogPage />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/w-d/:username"
+        element={
+          <Suspense fallback={<></>}>
+            <ProfilePage />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/blogs/:blogTitle"
+        element={
+          <Suspense fallback={<></>}>
+            <SingleBlogPage />
+          </Suspense>
+        }
+      />
       <Route
         path="/blog/create-blog"
         element={
-          <ProtectedRoutes user={user}>
-            <PostBuilderPage />{" "}
-          </ProtectedRoutes>
+          <Suspense fallback={<></>}>
+            <ProtectedRoutes user={user}>
+              <PostBuilderPage />{" "}
+            </ProtectedRoutes>
+          </Suspense>
         }
       />
       <Route
         path="/blog/edit-blog/:blogId"
         element={
-          <ProtectedRoutes user={user}>
-            <PostBuilderPage />{" "}
-          </ProtectedRoutes>
+          <Suspense fallback={<></>}>
+            {" "}
+            <ProtectedRoutes user={user}>
+              <PostBuilderPage />{" "}
+            </ProtectedRoutes>
+          </Suspense>
         }
       />{" "}
       <Route
         path="/admin/dashboard"
         element={
-          <AdminProtectedRoutes user={user}>
-            <AdminPage />{" "}
-          </AdminProtectedRoutes>
+          <Suspense fallback={<></>}>
+            {" "}
+            <AdminProtectedRoutes user={user}>
+              <AdminPage />{" "}
+            </AdminProtectedRoutes>
+          </Suspense>
         }
       />
     </Routes>
